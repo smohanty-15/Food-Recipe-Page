ce37fa3f13dcdf172b65873a87a71695c523086a
smohanty-15 <sandipanmohanty15@gmail.com>
2025-08-07 22:29:55 +0530

    add new

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+const searchBar = document.getElementById("searchBar");
+const cards = document.querySelectorAll(".card");
 
+searchBar.addEventListener("input", () => {
+  const query = searchBar.value.toLowerCase();
+  cards.forEach(card => {
+    const name = card.getAttribute("data-name").toLowerCase();
+    if (name.includes(query)) {
+      card.style.display = "block";
+    } else {
+      card.style.display = "none";
+    }
+  });
+});
